2eb467c715e0b37be01b172a34129d952de81df2
Sven Hempke <sweetnordic@users.noreply.github.com>
2022-04-11 21:11:59 +0200

    asp.net core code

---
+++ abschnitt-1/lösung/Workshop.WebApi/Controllers/EchoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Workshop.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+
+        [HttpGet("simple")]
+        public ActionResult<string> Simple() {
+            return Ok("Hallo Welt");
+        }
+
+        [HttpGet("parameter")]
+        public ActionResult<string> Parameter([FromQuery] string msg) {
+            return Ok(msg);
+        }
+
+        [HttpGet("validate/{msg}")]
+        public ActionResult<string> Validate([FromRoute] string msg = null) {
+            if (string.IsNullOrWhiteSpace(msg)) {
+                return BadRequest();
+            }
+            return Ok(msg);
+        }
+    }
+}
---
+++ abschnitt-1/lösung/Workshop.WebApi/Controllers/PersonsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Workshop.WebApi.Models;
+
+namespace Workshop.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+
+        private static Dictionary<int, Person> store { get; } = new Dictionary<int, Person> {
+            { 1, new Person{ Id = 1, Name = "Peter Peterson" } },
+            { 2, new Person{ Id = 2, Name = "Klaus Klausens" } },
+            { 3, new Person{ Id = 3, Name = "Singa Singable" } },
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> GetPersons() {
+            return Ok(store.Values.ToList());
+        }
+
+        [HttpGet("{id}", Name = nameof(GetPersonById))]
+        public ActionResult GetPersonById(int id) {
+            return Ok(store[id]);
+        }
+
+        [HttpPost]
+        public ActionResult PostPerson([FromBody] Person person) {
+            if (store.ContainsKey(person.Id)) { 
+                ModelState.AddModelError(nameof(person.Id), "Resource with the id already exists"); 
+            }
+            if (ModelState.IsValid) {
+                store.Add(person.Id, person);
+                return CreatedAtRoute(nameof(GetPersonById), new { person.Id }, person);
+            }
+            return BadRequest(ModelState.ValidationState);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult PutPerson([FromRoute] int id, [FromBody] Person person) {
+            if (!store.ContainsKey(id)) { return NotFound(); }
+            if (!ModelState.IsValid) { return BadRequest(ModelState.ValidationState); }
+            store[id] = person;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeletePerson([FromRoute] int id) {
+            if (!store.ContainsKey(id)) { return NotFound(); }
+            store.Remove(id);
+            return NoContent();
+        }
+
+    }
+}
